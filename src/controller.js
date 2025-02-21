12bc0b3f16743180f763915652f36c2444e24394
Liam Lee <liam.sho.lee1@gmail.com>
2025-02-21 21:40:44 +1000

    Detect player input

---
+++ src/controller.js
@@ -0,0 +1,49 @@
+const KEY = {
+    UP: 38,
+    DOWN: 40,
+    LEFT: 37,
+    RIGHT: 39,
+    SPACE: 32,
+    HELP: 72,
+    PAUSE: 80,
+    STORE: 83,
+    TOGGLE: 84,
+
+}
+
+export function handleKeyPress (event) {
+
+    event.preventDefault();
+    let key = event.which;
+
+    switch(key) {
+        case KEY.UP:
+            console.log("up");
+            break;
+        case KEY.DOWN:
+            console.log("down");
+            break;
+        case KEY.LEFT:
+            console.log("left");
+            break;
+        case KEY.RIGHT:
+            console.log("right");
+            break;
+        case KEY.SPACE:
+            console.log("space");
+            break;
+        case KEY.HELP:
+            console.log("help");
+            break;
+        case KEY.PAUSE:
+            console.log("pause");
+            break;
+        case KEY.STORE:
+            console.log("store");
+            break;
+        case KEY.TOGGLE:
+            console.log("toggle");
+            break;
+        
+    }
+}
--- src/index.js
+++ src/index.js
@@ -2,7 +2,12 @@ import "./styles.css";
 
 import { createBoard } from "./setup.js"
 
+import { handleKeyPress } from "./controller.js"
+
 createBoard(20, 10, ".board-section");
 
 let field = document.getElementsByClassName('block');
 console.log(field);
+
+document.addEventListener('keydown', handleKeyPress)
+
